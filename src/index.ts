fddec60110986956e3f5604ca71d731b7071c22f
himan1009 <shishusha922@gmail.com>
2024-04-17 13:37:42 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to DATABASE"));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// app.get("/test", async (req: Request, res: Response) => {
+//   res.json({ message: "HELLO !" });
+// });
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!!!!" });
+});
+
+// /api/my/user
+app.use("/api/my/user", myUserRoute);
+
+app.listen(7000, () => {
+  console.log("server started on local host : 7000");
+});
